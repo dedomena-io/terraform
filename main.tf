b731c90e59a08d9f4941402e702be68055c3b30f
Igor Grinkin <igrinkin@box.com>
2020-01-06 13:06:48 -0800

    adding S3 bucket for TF state files

--- main.tf
+++ main.tf
@@ -41,3 +41,23 @@ resource "aws_key_pair" "auth" {
   key_name   = var.key_name
   public_key = file(var.public_key_path)
 }
+
+# Remote state file
+resource "aws_s3_bucket" "dd_state" {
+  bucket = "dd-state"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+#terraform {
+#  required_version = ">= 0.9"
+#  backend "s3" {
+#    bucket = "dd-state"
+#    key    = "main/terraform.state"
+#    region = "us-west-2"
+#  }
+#}
