415af22dad63d355c78a0cd05793688497127f85
Igor Grinkin <igor@dedomena.io>
2020-01-13 00:51:09 +0000

    removing ECR roles; adding EKS roles; PROD-4

--- iam/output.tf
+++ iam/output.tf
@@ -1,5 +1,5 @@
 output "dd_dev_eks" { value = "${aws_iam_role.dd_dev_cluster.arn}" }
 output "dd_dev_policy" { value = "${aws_iam_role_policy_attachment.dd_dev_cluster_AmazonEKSClusterPolicy.id}" }
 output "dd_dev_service" { value = "${aws_iam_role_policy_attachment.dd_dev_cluster_AmazonEKSServicePolicy.id}" }
-output "dd_dev_node_arn" { value = "${aws_iam_role.dd_dev_node.arn}" }
-output "dd_dev_node_iam" { value = "${aws_iam_instance_profile.dd_dev_node.name}" }
+#output "dd_dev_node_arn" { value = "${aws_iam_role.dd_dev_node.arn}" }
+#output "dd_dev_node_iam" { value = "${aws_iam_instance_profile.dd_dev_node.name}" }
