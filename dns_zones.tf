0a5647f49d544eabacea5e8dd1efff2ad99ca4d3
Igor Grinkin <igor@dedomena.io>
2020-01-15 05:49:50 +0000

    moving to dns zones

--- dns_zones.tf
+++ dns_zones.tf
@@ -18,7 +18,7 @@ resource "aws_route53_record" "dev-krolm" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "dev.krolm.com"
   type    = "NS"
-  ttl     = "30"
+  ttl     = "${var.dns_ttl}"
 
   records = [
     "${aws_route53_zone.dev.name_servers.0}",
@@ -27,18 +27,3 @@ resource "aws_route53_record" "dev-krolm" {
     "${aws_route53_zone.dev.name_servers.3}",
   ]
 }
-
-# Public www records
-#resource "aws_route53_record" "apex" {
-#  zone_id = aws_route53_zone.main.zone_id
-#  name = "krolm.com"
-#  type = "A"
-#  alias {
-#    name = "www.krolm.com"
-#    zone_id = aws_route53_zone.main.zone_id
-#    evaluate_target_health = false
-#  }
-#  depends_on = [
-#    aws_route53_zone.main
-#  ]
-#}
